37fadca5a2b0f490552678262393bc037804a1ac
wewantwebsites <cas.guzdziol@wewantweb.site>
2024-04-21 12:50:36 -0400

    Add prerender flag to blog page

--- src/routes/blog/[...slug]/+page.ts
+++ src/routes/blog/[...slug]/+page.ts
@@ -1,11 +1,30 @@
+import type { Post } from '$lib/type.js';
 import { error } from '@sveltejs/kit';
 
+type Params = Post & { content: string };
+
+const postGlob = import.meta.glob('/src/posts/*.md');
+const fetchMap: Record<string, Params> = {};
 export async function load({ params, fetch }) {
 	try {
 		if (params?.slug) {
-			const file = await import(`/src/posts/${params.slug}.md`);
+			const cached = fetchMap[params.slug];
+			if (cached) {
+				return {
+					status: 304,
+					...cached
+				};
+			}
+			// Load the post content from the file system
+			const path = `/src/posts/${params.slug}.md`;
+			const file = (await postGlob[path]()) as { default: string };
 			const res = await fetch('/api/posts?slug=' + params.slug);
 			const data = await res.json();
+
+			fetchMap[params.slug] = {
+				...data,
+				content: file.default
+			};
 
 			return {
 				...data,
@@ -13,6 +32,7 @@ export async function load({ params, fetch }) {
 			};
 		}
 	} catch {
+		console.error('Could not find post ' + params.slug);
 		return error(404, 'Could not find post ' + params.slug);
 	}
 }
